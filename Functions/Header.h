01e38be58d718b24a2fe447f33328405aca00bd1
DeanCash <72015294+DeanCash@users.noreply.github.com>
2022-04-21 16:28:14 +0200

    addes files

---
+++ Functions/Header.h
@@ -0,0 +1,58 @@
+#ifndef Header_h
+#define Header_h
+
+#pragma once
+#include <iostream>
+#include <string>
+
+using std::string; using std::cout; using std::endl;
+
+namespace stringFunctions {
+	int strlen(string String) {
+		int i = 0;
+		while (String[i])
+			i++;
+		return i;
+	}
+
+	string ljust(string String, int inputAmount) {
+		int stringLength = strlen(String);
+		int amount = (inputAmount - stringLength);
+
+		if (inputAmount > 0 && inputAmount <= 100) {
+			for (int i = 0; i < amount; i++) {
+				String += " ";
+			}
+			return String;
+		} else if (inputAmount <= 0 || inputAmount > 100) {
+			cout << "Amount Cannot Be Bigger Than 100 Or Less Than 0!" << endl;
+			exit(1);
+		} else {
+			return String;
+		}
+	}
+
+	string rjust(string String, int inputAmount) {
+		int stringLength = strlen(String);
+		int amount = (inputAmount - stringLength);
+
+		string sampleString = "";
+		if (inputAmount > 0 && inputAmount <= 100) {
+			for (int i = 0; i < amount; i++) {
+				sampleString += " ";
+			}
+			sampleString += String;
+			string newString = sampleString;
+			return newString;
+		}
+		else if (inputAmount <= 0 || inputAmount > 100) {
+			cout << "Amount Cannot Be Bigger Than 100 Or Less Than 0!" << endl;
+			exit(1);
+		}
+		else {
+			return String;
+		}
+	}
+}
+
+#endif Header_h
