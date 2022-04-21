bd0f5f97eb989f20854b29ffbe3bf33a39f30eef
DeanCash <72015294+DeanCash@users.noreply.github.com>
2022-04-21 17:59:42 +0200

    Update stringFunctions.hpp

--- Functions/stringFunctions.hpp
+++ Functions/stringFunctions.hpp
@@ -14,29 +14,35 @@ namespace stringFunctions {
 		return i;
 	}
 
+	/* left justify the string - does te exact same thing as
+	   the ljust() in python, just takes in an extra arg  */
 	string ljust(string String, int inputAmount) {
 		int stringLength = strlen(String);
 		int amount = (inputAmount - stringLength);
 
-		if (inputAmount > 0 && inputAmount <= 100) {
+		// de adjust can't be bigger than 200
+		if (inputAmount > 0 && inputAmount <= 200) {
 			for (int i = 0; i < amount; i++) {
 				String += " ";
 			}
 			return String;
-		} else if (inputAmount <= 0 || inputAmount > 100) {
-			cout << "Amount Cannot Be Bigger Than 100 Or Less Than 0!" << endl;
+		} else if (inputAmount <= 0 || inputAmount > 200) {
+			cout << "Amount Cannot Be Bigger Than 200 Or Less Than 0!" << endl;
 			exit(1);
 		} else {
 			return String;
 		}
 	}
 
+	/* right justify the string - does te exact same thing as
+	   the rjust() in python, just takes in an extra arg   */
 	string rjust(string String, int inputAmount) {
 		int stringLength = strlen(String);
 		int amount = (inputAmount - stringLength);
-
 		string sampleString = "";
-		if (inputAmount > 0 && inputAmount <= 100) {
+
+		// de adjust can't be bigger than 200
+		if (inputAmount > 0 && inputAmount <= 200) {
 			for (int i = 0; i < amount; i++) {
 				sampleString += " ";
 			}
@@ -44,13 +50,13 @@ namespace stringFunctions {
 			string newString = sampleString;
 			return newString;
 		}
-		else if (inputAmount <= 0 || inputAmount > 100) {
-			cout << "Amount Cannot Be Bigger Than 100 Or Less Than 0!" << endl;
+		else if (inputAmount <= 0 || inputAmount > 200) {
+			cout << "Amount Cannot Be Bigger Than 200 Or Less Than 0!" << endl;
 			exit(1);
 		}
 		else {
 			return String;
 		}
 	}
-	
+
 }
