1e63616bb6a998bd43f68d80d71155fa1b3ed736
DeanCash <72015294+DeanCash@users.noreply.github.com>
2022-05-01 23:10:45 +0200

    rewrote some code

--- Functions/stringFunctions.hpp
+++ Functions/stringFunctions.hpp
@@ -9,14 +9,6 @@ using std::string; using std::cout; using std::endl;
 using std::filesystem::path;
 
 namespace stringFunctions {
-	
-	// int strlen(string String)
-	// {
-	// 	int i = 0;
-	// 	while (String[i])
-	// 		i++;
-	// 	return i;
-	// }
 
 	/* left justify the string - does te exact same thing as
 	   the ljust() in python, just takes in an extra arg    */
@@ -25,15 +17,15 @@ namespace stringFunctions {
 		int stringLength = String.length();
 		int amount = (inputAmount - stringLength);
 
-		// de adjust can't be bigger than 200
-		if (inputAmount > 0 && inputAmount <= 200) {
+		// de adjust can't be bigger than 300
+		if (inputAmount > 0 && inputAmount <= 300) {
 			for (int i = 0; i < amount; i++) {
 				String += " ";
 			}
 			return String;
-		} else if (inputAmount <= 0 || inputAmount > 200) {
-			cout << "Amount Cannot Be Bigger Than 200 Or Less Than 0!" << endl;
-			exit(1);
+		} else if (inputAmount <= 0 || inputAmount > 300) {
+			cout << "Amount Cannot Be Bigger Than 300 Or Less Than 0!" << endl;
+			return "";;
 		} else {
 			return String;
 		}
@@ -47,8 +39,8 @@ namespace stringFunctions {
 		int amount = (inputAmount - stringLength);
 		string sampleString = "";
 
-		// de adjust can't be bigger than 200
-		if (inputAmount > 0 && inputAmount <= 200) {
+		// de adjust can't be bigger than 300
+		if (inputAmount > 0 && inputAmount <= 300) {
 			for (int i = 0; i < amount; i++) {
 				sampleString += " ";
 			}
@@ -56,9 +48,9 @@ namespace stringFunctions {
 			string newString = sampleString;
 			return newString;
 		}
-		else if (inputAmount <= 0 || inputAmount > 200) {
-			cout << "Amount Cannot Be Bigger Than 200 Or Less Than 0!" << endl;
-			exit(1);
+		else if (inputAmount <= 0 || inputAmount > 300) {
+			cout << "Amount Cannot Be Bigger Than 300 Or Less Than 0!" << endl;
+			return "";;
 		}
 		else {
 			return String;
